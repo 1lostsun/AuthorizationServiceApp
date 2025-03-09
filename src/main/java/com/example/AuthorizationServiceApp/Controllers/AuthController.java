bb1da01ccf152b7f672e4f18bff8f31ee6391fdf
1lostsun <149478863+1lostsun@users.noreply.github.com>
2025-03-10 00:03:03 +0300

    message sending update, dto update

--- src/main/java/com/example/AuthorizationServiceApp/Controllers/AuthController.java
+++ src/main/java/com/example/AuthorizationServiceApp/Controllers/AuthController.java
@@ -7,11 +7,15 @@ import com.example.AuthorizationServiceApp.Services.JWT.AuthService;
 import com.example.AuthorizationServiceApp.Services.Kafka.KafkaProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Stack;
 
 @RestController()
 @RequestMapping("/auth")
@@ -28,28 +32,28 @@ public class AuthController {
 	@PostMapping("/register")
 	public ResponseEntity<?> register(@RequestBody UserRequestDto userRequestDto, RedirectAttributes redirectAttributes) throws JsonProcessingException {
 		authService.registerUser(userRequestDto);
-		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(),"User registered successfully with email: " + userRequestDto.getEmail(), "Welcome in our App"));
+		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(),"register" ,"User registered successfully with email: " + userRequestDto.getEmail(), "Welcome in our App"));
 		return ResponseEntity.status(201).body("User registered successfully");
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody UserRequestDto userRequestDto) throws JsonProcessingException {
 		AuthResponseDto authResponseDto = authService.authenticateUser(userRequestDto.getEmail(), userRequestDto.getPassword());
-		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(), "User logged in: " + userRequestDto.getEmail(), "You logged in"));
+		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(), "login" ,"User logged in: " + userRequestDto.getEmail(), "You logged in"));
 		return ResponseEntity.ok(authResponseDto);
 	}
 
 	@PostMapping("/logout")
 	public ResponseEntity<?> logout(@RequestBody UserRequestDto userRequestDto) throws JsonProcessingException {
 		authService.logoutUser(userRequestDto.getUsername());
-		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(),"User logout successfully", "You logout successfully"));
+		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(), "logout" ,"User logout successfully", "You logout successfully"));
 		return ResponseEntity.ok("User logout successfully");
 	}
 
 	@PostMapping("/refresh")
 	public ResponseEntity<AuthResponseDto> refreshToken(@RequestBody UserRequestDto userRequestDto) throws JsonProcessingException {
 		AuthResponseDto newTokens = authService.refreshToken(userRequestDto.getUsername());
-		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(),"User refreshed token", "Thanks for staying with us"));
+		kafkaProducer.sendMessage("auth-topic", null, new MessageDto(userRequestDto.getEmail(), userRequestDto.getUsername(),"refresh", "User refreshed token", "Thanks for staying with us"));
 		return ResponseEntity.ok(newTokens);
 	}
 }
--- src/main/java/com/example/AuthorizationServiceApp/Dto/MessageDto.java
+++ src/main/java/com/example/AuthorizationServiceApp/Dto/MessageDto.java
@@ -4,12 +4,14 @@ public class MessageDto {
 
 	private String to;
 	private String username;
+	private String template;
 	private String subject;
 	private String body;
 
-	public MessageDto(String to, String username, String subject, String body) {
+	public MessageDto(String to, String username, String template, String subject, String body) {
 		this.to = to;
 		this.username = username;
+		this.template = template;
 		this.subject = subject;
 		this.body = body;
 	}
@@ -20,6 +22,10 @@ public class MessageDto {
 
 	public String getUsername() {
 		return username;
+	}
+
+	public String getTemplate() {
+		return template;
 	}
 
 	public String getSubject() {
